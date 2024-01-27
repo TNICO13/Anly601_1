db511ece1955725e5988fad4585810703e3794a3
Tyler <tylernico@tamu.edu>
2024-01-27 00:07:11 -0500

    Final Commit

---
+++ Fuel Econ Calc.py
@@ -0,0 +1,75 @@
+import requests  #Used to request information from a website
+import tkinter as tk       # Used to pull original tkinter objects
+from   tkinter import ttk  # Used for new Theme'd Objects
+# from tkinter import *     # Alternate approach to creating the GUI canvas
+import tkinter as tk  # Used to pull original tkinter objects
+from tkinter import ttk  # Used for new Theme'd Objects
+import ttkthemes
+
+def Calculate():
+    Miles_Driven = float(Miles_Driven_Label.get())
+    Fuel_Consumption = float(Fuel_Consumption_Label.get())
+    MPG = round(float(Miles_Driven / Fuel_Consumption), 2)
+    MPG_Label.set(MPG)
+
+Miles_Driven = float(12)
+Fuel_Consumption = float(3)
+MPG = float(Miles_Driven / Fuel_Consumption)
+
+
+# def calc():
+# MD = float(Miles_Driven.get())
+# FC = float(Fuel_Consumption.get())
+# MPG = float(MD / FC)
+
+
+
+root  = tk.Tk()
+root.configure(bg='white')
+root.title("Fuel Economy Calculator")
+root.geometry('380x200+1000+300') #Set Window Size (WxH) & Location (+X+Y)
+style = ttk.Style()
+style.theme_use('clam')
+style.configure("TButton", foreground="maroon", background="white", font="Arial, 15")
+style.configure("TEntry", fieldbackground='maroon', foreground="white")
+style.configure("TLabel", background='white')
+
+
+
+
+Miles_Driven_Label = ttk.Label(root, text = "Miles Driven:").grid(row=0, column=0, sticky="w")
+Miles_Driven_Label = tk.StringVar()
+Miles_Driven_Label.set(Miles_Driven)
+entry  = ttk.Entry(root, textvariable=Miles_Driven_Label, width=10,font="Arial, 18", justify='right').grid(row=0, column=2)
+
+
+Fuel_Consumption_Label = ttk.Label(root, text = "Fuel Consumption:").grid(row=1, column=0, sticky="w")
+Fuel_Consumption_Label = tk.StringVar()
+Fuel_Consumption_Label.set(Fuel_Consumption)
+entry  = ttk.Entry(root, textvariable=Fuel_Consumption_Label, width=10, font="Arial, 18", justify='right').grid(row=1, column=2)
+
+
+frame_break = ttk.Frame(root, height=2, relief="sunken", borderwidth=1)
+frame_break.grid(row=3, column=0, sticky="ew", pady=5)
+
+frame_break = ttk.Frame(root, height=2, relief="sunken", borderwidth=1)
+frame_break.grid(row=3, column=1, sticky="ew", pady=5)
+
+frame_break = ttk.Frame(root, height=2, relief="sunken", borderwidth=1)
+frame_break.grid(row=3, column=2, sticky="ew", pady=5)
+
+spacer_frame = ttk.Frame(root, width=20, height=10, style="TLabel")  # Width is the space width
+spacer_frame.grid(row=4, column=1)
+
+
+MPG_Label = ttk.Label(root, text = "MPG:", anchor='e').grid(row=2, column=1)
+MPG_Label = tk.StringVar()
+MPG_Label.set(MPG)
+entry  = ttk.Entry(root, textvariable=MPG_Label, width=10, font="Arial, 18", justify='right').grid(row=2, column=2)
+
+
+
+calculate_button = ttk.Button(root, text="Calculate", command=Calculate).grid(row=5, column=1)
+
+
+root.mainloop()
--- Main.py
+++ Main.py
@@ -1,24 +1,48 @@
-import requests, json      # Used to request information from a website
-#from tkinter import *     # Alternate approach to creating the GUI canvas
+import requests  #Used to request information from a website
 import tkinter as tk       # Used to pull original tkinter objects
 from   tkinter import ttk  # Used for new Theme'd Objects
+# from tkinter import *     # Alternate approach to creating the GUI canvas
+import tkinter as tk  # Used to pull original tkinter objects
+from tkinter import ttk  # Used for new Theme'd Objects
+
+import requests  # Used to request information from a website
+
+def refresh():
+    symbol = stock_symbol.get()
+    url = base_url + query + "&symbol=" + symbol + "&apikey=" + api_key
+    print("Request Sent to: \n", url, '\n')
+    r = requests.get(url)
+    data = r.json()
+
+    # Extract Stock Values from Global Quote
+    symbol = data["Global Quote"]["01. symbol"]  # Extract stock symbol
+    high = data["Global Quote"]["03. high"]  # Extract highest price
+    price = data["Global Quote"]["05. price"]  # Extract closing price
+    day = data["Global Quote"]["07. latest trading day"]  # Trading Day
+    high = '$'+str(round(float(high),2))
+    price = '$'+str(round(float(price),2))
+    print(symbol, day, high, price, "\n")
+    high_price.set(high)
+    close_price.set(price)
+
 
 base_url = "https://www.alphavantage.co/query?function="
 query   = "GLOBAL_QUOTE"
-symbol  = "IBM"
+symbol  = "MSFT"
 api_key = "IZHAYZFSN4A3I5OY"
+
 
 url = base_url + query + "&symbol=" + symbol + "&apikey=" + api_key
 print("Request Sent to: \n", url, '\n')
 r    = requests.get(url)
 data = r.json()
 
-#print Alpha Vantage data dictionary
+# print Alpha Vantage data dictionary
 print("Alpha Vantage Returned Data:\n", data, "\n")
 #print "Global Quote" Dictionary Contents
 print("Global Quote Contents:\n", data["Global Quote"],"\n")
 
-#Extract Stock Values from Global Quote
+# Extract Stock Values from Global Quote
 symbol = data["Global Quote"]["01. symbol"]#Extract stock symbol
 high   = data["Global Quote"]["03. high"]  #Extract highest price
 price  = data["Global Quote"]["05. price"] #Extract closing price
@@ -39,8 +63,12 @@ stock_entry  = ttk.Entry(root, textvariable=stock_symbol, width=10,
 stock_high = ttk.Label(root, text = "Price High:").\
                         grid(row=1, column=0, sticky="w")
 stock_high = tk.StringVar()
-stock_high.set(price)
+stock_high.set(high)
 stock_entry  = ttk.Entry(root, textvariable=stock_high, width=10,
                font="Arial, 18", justify='center').grid(row=1, column=1)
+
+
+# Now you can create the button with the refresh command.
+refresh_button = ttk.Button(root, text="Refresh", command=refresh).grid(row=3, column=1)
 
 root.mainloop()
